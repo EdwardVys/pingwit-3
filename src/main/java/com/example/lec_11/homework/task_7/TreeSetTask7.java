b80c9e3df798372ec0c967f9aee1a1b2771761de
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-15 08:57:25 +0200

    Update TreeSetTask7.java

--- src/main/java/com/example/lec_11/homework/task_7/TreeSetTask7.java
+++ src/main/java/com/example/lec_11/homework/task_7/TreeSetTask7.java
@@ -5,7 +5,7 @@ import java.util.TreeSet;
 
 public class TreeSetTask7 {
     public static void main(String[] args) {
-        TreeSet<Integer> treetask7 = new TreeSet<Integer>();
+        TreeSet<Integer> treetask7 = new TreeSet<Integer>(); // лучше не использовать цифры в названии переменных и классах тоже
         TreeSet<Integer> reversetreetask7 = new TreeSet<Integer>();
 
         treetask7.add(14);
@@ -13,11 +13,11 @@ public class TreeSetTask7 {
         treetask7.add(5);
         treetask7.add(11);
         treetask7.add(55);
-        treetask7.add(97);  // Добавляем данные
+        treetask7.add(97);  // Добавляем данные - комментарии в финальном варианте не стоит оставлять
 
         System.out.println(treetask7);
 
-        reversetreetask7 = (TreeSet) treetask7.descendingSet();// разворачиваем
+        reversetreetask7 = (TreeSet) treetask7.descendingSet();// разворачиваем - комментарии в финальном варианте не стоит оставлять
 
         System.out.println(reversetreetask7);
     }
