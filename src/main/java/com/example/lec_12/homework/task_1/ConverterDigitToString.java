929c8d0137b1d603cdb6d4cc8b79b58cda8c143b
Alexander Fomin <RezviyBelorus@gmail.com>
2022-04-15 09:38:21 +0200

    Update ConverterDigitToString.java

--- src/main/java/com/example/lec_12/homework/task_1/ConverterDigitToString.java
+++ src/main/java/com/example/lec_12/homework/task_1/ConverterDigitToString.java
@@ -2,15 +2,28 @@ package com.example.lec_12.homework.task_1;
 
 public class ConverterDigitToString {
     public static void main(String[] args) {
+        // Function<Integer, String> converter = x -> String.valueOf(x);
+        // ==
+        // Function<Integer, String> converter = String::valueOf;
         Function<Integer, String> Converter = x -> {
             return String.valueOf(x);
         };
         System.out.println(Converter.apply(66) + " рупий");
+        
+        // вы просто из числа 66 делаете строку "66" и добавляете рупий в System.out.println().
+        // ваша функция как раз должна принимать 2 аргумента, а возвращать 1 
+        // пример
+        /*
+        BiFunction<Integer, String, String> converter = (number, currency) -> number + " " + currency;
+        String result = converter.apply(66, "рупий");
+
+        System.out.println(result);
+        */
     }
 }
 
 @FunctionalInterface
-interface Function<T, R> {
+interface Function<T, R> { // вот прям точно такой же интерфейс есть в джаве, этот удаляйте
     R apply(T t);
 
     default void sayHi() {
